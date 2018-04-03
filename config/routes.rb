46fde58d6c5a538e190a718d0b559504437caef2
Alexandria Brown <axbn2190@gmail.com>
2018-04-03 14:17:02 -0700

    routes troubleshooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,4 @@
 Rails.application.routes.draw do
-  get 'trips/index'
-
-  get 'trips/create'
-
-  get 'trips/destory'
-
-  get 'trips/edit'
-
-  get 'trips/new'
-
-  get 'trips/show'
-
-  get 'passengers/index'
-
-  get 'passengers/create'
-
-  get 'passengers/destory'
-
-  get 'passengers/edit'
-
-  get 'passengers/new'
-
-  get 'passengers/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :drivers
