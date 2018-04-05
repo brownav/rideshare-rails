7a7f99b73503f86025bed670481144869a90acf4
Lily Sky <lilyxsky@gmail.com>
2018-04-05 15:20:53 -0700

    rating method for driver done + linked on driver show

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,15 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :vin, presence: true, uniqueness: true
+
+  def avg_rating
+    total_rating = 0
+    self.trips.all.each do |trip|
+      total_rating += trip.rating
+    end
+    average_rating = total_rating/self.trips.length.to_f
+    return average_rating.round(1)
+  end
+
+
 end
