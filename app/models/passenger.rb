ccc2e757b6f780f24fdf4605a4db005aecc4b7f4
Lily Sky <lilyxsky@gmail.com>
2018-04-05 15:38:18 -0700

    passenger paid method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,13 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :phone_num, presence: true
+
+  def total_paid
+    total = 0
+    self.trips.each do |trip|
+      total += trip.cost
+    end
+
+    return total.to_f/100.round(2)
+  end
 end
