f060c8a5e93a5c09a4a2f2d1786850e21430d783
Alexandria Brown <axbn2190@gmail.com>
2018-04-06 15:51:45 -0700

    cleaned nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :drivers do
-    resources :trips, only: [:show, :edit, :destroy]
+    resources :trips, only: [:show, :destroy]
   end
 
   resources :passengers do
-    resources :trips, only: [:show, :edit, :destroy, :new]
+    resources :trips, only: [:show, :destroy, :new]
   end
 
   resources :trips
-  
+
   root 'trips#index'
 
 end
